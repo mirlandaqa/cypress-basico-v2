def65bf1a4ad134881a1c0b066d3c6243a1f6797
Mirlanda Costa <mirlanda.dsc@gmail.com>
2024-04-17 13:00:04 -0400

    Create cypress project

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+  const { defineConfig } = require("cypress");
+  module.exports = defineConfig({
+    viewportWidth: 1280,  
+    viewportHeight: 880,
+    e2e: {
+
+      setupNodeEvents(on, config)
+      {
+        //"pluginsFile":false,
+      }
+    }
+  });
